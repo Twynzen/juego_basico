3853581f806c2f4a03868a0a22b5672a618f8e58
Daniel <razzziel001@gmail.com>
2019-08-24 19:32:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,28 +6,82 @@ var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth)/2;
+var rightPressed = false;
+var leftPressed = false;
+var velball = 8;
 
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = "green";
     ctx.fill();
     ctx.closePath();
 }
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
     x += dx;
     y += dy;
     //si golpea la parte superior, invertir la dirección de la pelota
-    if(y + dy > canvas.height || y + dy < 0) {
-    dy = -dy;
+    if(y + dy < ballRadius) {
+      dy = -dy;
+  } else if(y + dy > canvas.height-ballRadius) {
+      if(x > paddleX && x < paddleX + paddleWidth) {
+        velball--;
+          dy = -dy;
+
+      }
+      else {
+        
+          alert("Perdiste preciosaaa");
+          document.location.reload();
+      }
   }
     if(x + dx > canvas.width || x + dx < 0) {
     dx = -dx;
   }
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 5;
+}
+else if(leftPressed && paddleX > 0) {
+    paddleX -= 5;
+ }
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
 //para repetir un ntervalo de forma establecida
-setInterval(draw, 2);
+
+
+setInterval(draw, velball);

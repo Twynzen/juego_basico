3797f4a8477a681a523702842e21bd910685cbda
Daniel <razzziel001@gmail.com>
2019-08-30 23:11:29 -0500

    Update scripts.js
    Aumento la velocidad por cada golpe en la barra

--- scripts.js
+++ scripts.js
@@ -11,55 +11,7 @@ var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
-var velball = 8;
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawPaddle();
-    drawBall();
-    x += dx;
-    y += dy;
-    //si golpea la parte superior, invertir la dirección de la pelota
-    if(y + dy < ballRadius) {
-      dy = -dy;
-  } else if(y + dy > canvas.height-ballRadius) {
-      if(x > paddleX && x < paddleX + paddleWidth) {
-        velball--;
-          dy = -dy;
-
-      }
-      else {
-        
-          alert("Perdiste preciosaaa");
-          document.location.reload();
-      }
-  }
-    if(x + dx > canvas.width || x + dx < 0) {
-    dx = -dx;
-  }
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 5;
-}
-else if(leftPressed && paddleX > 0) {
-    paddleX -= 5;
- }
-}
-
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-}
+var velball = 20;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -81,6 +33,66 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
+    drawBall();
+    x += dx;
+    y += dy;
+    //si golpea la parte superior, invertir la dirección de la pelota
+    if(y + dy < ballRadius) {
+      dy = -dy;
+  } else if(y + dy > canvas.height-ballRadius) {
+
+      vallmas()
+      if(x > paddleX && x < paddleX + paddleWidth) {
+
+          dy = -dy;
+
+      }
+      else {
+
+          alert("Perdiste preciosaaa");
+          document.location.reload();
+      }
+  }
+    if(x + dx > canvas.width || x + dx < 0) {
+    dx = -dx;
+  }
+  if(rightPressed && paddleX < canvas.width-paddleWidth) {
+    paddleX += 5;
+}
+else if(leftPressed && paddleX > 0) {
+    paddleX -= 5;
+ }
+}
+function vallmas() {
+  velball--;
+  setInterval(draw, velball);
+  console.log(velball);
+}
+
+
+
+
+
 //para repetir un ntervalo de forma establecida
 
 
